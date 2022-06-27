5ae4e6b151210711574a2bf5cfde0a4610565691
Julia <granovskaya.97@mail.ru>
2022-06-27 21:31:27 +0300

    Add randomNumber function

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Вывести случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
+void randomNumber(){
+    int number = new Random().Next(10, 100);
+    Console.Write(number + "->");
+    if (number / 10 > number % 10)
+        {
+        Console.WriteLine(number / 10);
+        }
+    else
+        {
+        Console.WriteLine(number % 10);
+        }
+}
+randomNumber();
