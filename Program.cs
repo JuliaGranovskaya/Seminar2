90c076a2c35497cf03a79a656c0550a6bc897b93
Julia <granovskaya.97@mail.ru>
2022-06-30 16:50:40 +0300

    Add tasks

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// Вывести случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
-// void randomNumber(){
-//     int number = new Random().Next(10, 100);
+// void randomNumber(int min, int max){
+//     int number = new Random().Next(min, max+1);
 //     Console.Write(number + "->");
 //     if (number / 10 > number % 10)
 //         {
@@ -11,7 +11,26 @@
 //         Console.WriteLine(number % 10);
 //         }
 // }
-// randomNumber();
+// Console.WriteLine("Введите минимальное число диапазона: ");
+// int start = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимальное число диапазона: ");
+// int end = Convert.ToInt32(Console.ReadLine());
+// randomNumber(start,end);
+
+// Вывести сумму двух чисел
+// int SumTwoNumbers()
+// {
+//     Console.WriteLine("Введите первое число: ");
+//     int number1 = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Введите второе число: ");
+//     int number2 = Convert.ToInt32(Console.ReadLine());
+//     int sum = number1 + number2;
+//     return sum;
+// }
+// for (int i=0; i < 3; i++)
+// {
+//     Console.WriteLine("Сумма двух чисел: "+ SumTwoNumbers());
+// }
 
 // Домашнее задание
 
@@ -26,20 +45,24 @@
 //         Console.WriteLine(""+ number +" -> "+ result +"");
 //         }
 //     else Console.WriteLine("Некорректное значение");
-//     }   
+//     }
 // secondNumber();
 
 // Вывести третью цифру заданного числа или написать что ее нет
 // void thirdNumber()
 //     {
-//     Console.Write("Введите трехзначное число: " );
+//     Console.Write("Введите число: " );
 //     int number = Convert.ToInt32(Console.ReadLine());
-//     if (number > 99 && number < 1000)
+//     Console.Write(""+ number +" -> ");
+//     if (number > 99)
 //         {
+//         while (number / 1000 > 0)
+//             {number = number / 10;
+//             };
 //         int result = number % 10;
-//         Console.WriteLine(""+ number +" -> "+ result +"");
+//         Console.Write(result);
 //         }
-//     else Console.WriteLine(""+ number +" -> третьей цифры нет");
+//     else Console.Write("третьей цифры нет");
 //     }   
 // thirdNumber();
 
